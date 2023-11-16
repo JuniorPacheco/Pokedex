5fa82342327c87134d411a5b0877a0ce651c014d
Junior Pacheco <junior.pacheco.356@gmail.com>
2023-11-16 17:34:06 -0500

    Pokemon filter by name and infinite scroll

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,18 +1,47 @@
 import { IconSearch } from "@tabler/icons-react";
 import axios from "axios";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useEffect } from "react";
 import PokemonList from "./PokemonList";
+import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
+
+const INITIAL_LIMIT = 40;
+const INCREASE_LIMIT = 20;
 
 const Pokemons = () => {
   const [allPokemons, setAllPokemons] = useState([]);
+  const [pokemonName, setPokemonName] = useState("");
+  const [limit, setLimit] = useState(INITIAL_LIMIT);
+
+  const targetObserver = useRef(null);
+  const entry = useIntersectionObserver(targetObserver, {});
+  const isVisible = !!entry?.isIntersecting;
+
+  const pokemonsByName = allPokemons.filter((pokemon) =>
+    pokemon.name.includes(pokemonName)
+  );
+
+  const handleChangePokemonName = (e) =>
+    setPokemonName(e.target.value.toLowerCase());
 
   useEffect(() => {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=50")
+      .get("https://pokeapi.co/api/v2/pokemon?limit=898")
       .then(({ data }) => setAllPokemons(data.results))
       .catch((err) => console.log(err));
   }, []);
+
+  useEffect(() => {
+    if (isVisible) {
+      const maxPokemons = pokemonsByName.length;
+      const newLimit = limit + INCREASE_LIMIT;
+      newLimit > maxPokemons ? setLimit(maxPokemons) : setLimit(newLimit);
+    }
+  }, [isVisible]);
+
+  useEffect(() => {
+    setLimit(INITIAL_LIMIT);
+  }, [pokemonName]);
 
   return (
     <section className="p-4 py-5">
@@ -21,15 +50,27 @@ const Pokemons = () => {
           <input
             className="outline-none flex-1"
             type="text"
+            autoComplete="off"
             placeholder="Search your Pokemon"
+            name="pokemonName"
+            onChange={handleChangePokemonName}
           />
           <button className="bg-red-500 p-2 rounded-xl shadow-lg shadow-red-500/50 hover:bg-red-400 transition-colors">
             <IconSearch color="white" stroke={3} />
           </button>
         </div>
       </form>
-      <PokemonList pokemons={allPokemons} />
+      <PokemonList pokemons={pokemonsByName.slice(0, limit)} />
+
+      {/* Target Observer */}
+      <span ref={targetObserver}></span>
     </section>
   );
 };
 export default Pokemons;
+
+//? Forma no controlada
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+//   setPokemonName(e.target.pokemonName.value.toLowerCase());
+// };
